e40716aa0006835e3fa6cca00f442f19943e54a3
W61486 <W61486@RA250B-10>
2019-01-28 18:05:31 +0100

    Console app ready

--- Dice/Dice.Console/Program.cs
+++ Dice/Dice.Console/Program.cs
@@ -11,7 +11,22 @@ namespace DiceConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Enter how many dices you want to roll: ");
+            int roll = Convert.ToInt32(Console.ReadLine());
+            Die die = new Die();
+            for (int i = 0; i < roll; i++) 
+            {
+                die.Roll();
+                int val = die.Show();
+                int t = roll - i;
+                if (t <= 1)
+                {
+                    Console.WriteLine(val + " ");
+                } else
+                Console.Write(val + ", ");             
+            }
+            
+            
             
         }
     }
