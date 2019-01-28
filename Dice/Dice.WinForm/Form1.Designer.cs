1af3fe95e0d526a7b6e9f7d6defec293d7275edf
Katarzyna Fir <kassfazuya@gmail.com>
2019-01-28 18:11:38 +0100

    Windows Forms

--- Dice/Dice.WinForm/Form1.Designer.cs
+++ Dice/Dice.WinForm/Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rollButton = new System.Windows.Forms.Button();
+            this.rolledDiceTextBox = new System.Windows.Forms.TextBox();
+            this.rolledLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(131, 281);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(139, 33);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "ROLL THE DICE";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // rolledDiceTextBox
+            // 
+            this.rolledDiceTextBox.Location = new System.Drawing.Point(83, 177);
+            this.rolledDiceTextBox.Name = "rolledDiceTextBox";
+            this.rolledDiceTextBox.Size = new System.Drawing.Size(187, 20);
+            this.rolledDiceTextBox.TabIndex = 1;
+            // 
+            // rolledLabel
+            // 
+            this.rolledLabel.AutoSize = true;
+            this.rolledLabel.Location = new System.Drawing.Point(12, 184);
+            this.rolledLabel.Name = "rolledLabel";
+            this.rolledLabel.Size = new System.Drawing.Size(65, 13);
+            this.rolledLabel.TabIndex = 2;
+            this.rolledLabel.Text = "Rolled Dice:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "It\'s time to roll some dice!";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(12, 116);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(186, 13);
+            this.numberLabel.TabIndex = 4;
+            this.numberLabel.Text = "How many dice do you wish to throw?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(204, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rolledLabel);
+            this.Controls.Add(this.rolledDiceTextBox);
+            this.Controls.Add(this.rollButton);
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DICE";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.TextBox rolledDiceTextBox;
+        private System.Windows.Forms.Label rolledLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Dice/Dice.WinForm/Form1.cs
+++ Dice/Dice.WinForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DieLogic;
 
 namespace Dice.WinForm
 {
@@ -15,6 +16,23 @@ namespace Dice.WinForm
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(new Object []{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void rollButton_Click(object sender, EventArgs e)
+        {
+            var numberOfDice = (int)comboBox1.SelectedItem;
+            Die die = new Die();
+            rolledDiceTextBox.Clear();
+            for (int eachDie = 0; eachDie < numberOfDice; eachDie++)
+            {
+                die.Roll();
+                var result = die.Show();
+                rolledDiceTextBox.Text = rolledDiceTextBox.Text + " " + result.ToString();
+            }
+
         }
     }
 }
