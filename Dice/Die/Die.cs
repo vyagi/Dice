d4682ff4b21fd403099588cf7dbf7310ca14ea44
Lubiv <lubivartur@gmail.com>
2019-01-28 16:51:35 +0100

    changed namespace

--- Dice/Die/Die.cs
+++ Dice/Die/Die.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Die
+namespace DieLogic
 {
     public class Die
     {
+        private int 
         public void Roll()
         {
 
